06a95cf264b96ad5c1cf59e10b18698b443b2789
Marcel Zielinski <marziel2001@gmail.com>
2025-07-24 21:27:33 +0200

    move app.css to css dir

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import Layout from "./Layout";
 import Home from "./home";
 import NoPage from "./NoPage";
