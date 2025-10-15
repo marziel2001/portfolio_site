22c3386f28f3b9cb9c913e8055609bfc0af1feeb
Marcel Zielinski <marziel2001@gmail.com>
2025-10-16 00:04:45 +0200

    Pad main section of the page

---
+++ portfolio-site/src/components/pagePadding.tsx
@@ -0,0 +1,14 @@
+import React, { type ReactNode } from 'react';
+
+interface PagePaddingProps {
+    children: ReactNode;
+    className?: string;
+}
+
+const PagePadding: React.FC<PagePaddingProps> = ({ children, className = '' }) => (
+    <div className={`px-4 py-8 md:px-12 lg:px-24 ${className}`}>
+        {children}
+    </div>
+);
+
+export default PagePadding;
