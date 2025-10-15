97d044cdc8bc0060a8bde8ed26db0c7ba262a436
Marcel Zielinski <marziel2001@gmail.com>
2025-10-15 23:46:39 +0200

    Add dynamic image loading

--- portfolio-site/src/components/photoMosaic.tsx
+++ portfolio-site/src/components/photoMosaic.tsx
@@ -1,30 +1,35 @@
-import React from "react";
-import im1 from "../staticImages/image1.jpg";
-import im2 from "../staticImages/image2.jpg";
-import im3 from "../staticImages/image3.jpg";
+// import React from "react";
+// import im1 from "../staticImages/image1.jpg";
+// import im2 from "../staticImages/image2.jpg";
+// import im3 from "../staticImages/image3.jpg";
 
-const photos = [
-  { src: im1, width: 4, height: 3 },
-  { src: im2, width: 3, height: 2 },
-  { src: im3, width: 1, height: 1 },
-];
+// const photos = [
+//   { src: im1, width: 4, height: 3 },
+//   { src: im2, width: 3, height: 2 },
+//   { src: im3, width: 1, height: 1 },
+// ]; classic version to replace with rest calls
+
+const images = import.meta.glob("../../public/staticImages/**/*.{jpg,jpeg,png,webp}", { eager: true });
+
+const imageList = Object.values(images).map((mod) => (mod as { default: string }).default);
 
 const MAX_HEIGHT = 200;
 
 const PhotoMosaic = () => {
+
+  console.log(imageList);
+
   return (
     <div className="flex flex-wrap justify-center gap-2">
-      {photos.map((photo, idx) => {
-        const aspectRatio = photo.width / photo.height;
-        const width = MAX_HEIGHT * aspectRatio;
+      {imageList.map((src, idx) => {
         return (
           <div
             key={idx}
             className="rounded-md shadow-md bg-cover bg-center"
             style={{
-              backgroundImage: `url(${photo.src})`,
-              width: `${width}px`,
-              height: `${MAX_HEIGHT}px`,
+              backgroundImage: `url(${src})`,
+              width: '200px',
+              aspectRatio: '3 / 2',
             }}
           ></div>
         );
