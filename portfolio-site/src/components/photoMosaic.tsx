659585fc7114d227ce659537f94a680cd44a35fd
Marcel Zielinski <marziel2001@gmail.com>
2025-10-31 18:07:25 +0100

    Add loading images statically using info saved in json, which will enable app to use thumbnails and fullsize images when needed

--- portfolio-site/src/components/photoMosaic.tsx
+++ portfolio-site/src/components/photoMosaic.tsx
@@ -3,6 +3,8 @@
 // import im2 from "../staticImages/image2.jpg";
 // import im3 from "../staticImages/image3.jpg";
 
+import gallery from "../../public/staticImages/gallery.json";
+
 // const photos = [
 //   { src: im1, width: 4, height: 3 },
 //   { src: im2, width: 3, height: 2 },
@@ -12,43 +14,45 @@
 // const images = import.meta.glob("../assets/staticImages/**/*.{jpg,jpeg,png,webp}", { eager: true });
 // const imageList = Object.values(images).map((mod) => (mod as { default: string }).default);
 
-const imageList: string[] = await (async () => {
-  try {
-    const res = await fetch("http://localhost:3000/photos", { method: "GET" });
-    if (!res.ok) return ["blad"];
-    const data = await res.json();
-    if (!Array.isArray(data)) return ["blad"];
-    return data
-      .map((item) => {
-        if (typeof item === "string") return item;
-        if (item == null) return "jakis blad";
-        return (item as any).filename ?? (item as any).src ?? (item as any).path ?? "error";
-      })
-      .filter(Boolean);
-  } catch {
-    return [];
-  }
-})();
+console.log(gallery);
+
+// const imageList: string[] = await (async () => {
+//   try {
+//     const res = await fetch("http://localhost:3000/photos", { method: "GET" });
+//     if (!res.ok) return ["blad"];
+//     const data = await res.json();
+//     if (!Array.isArray(data)) return ["blad"];
+//     return data
+//       .map((item) => {
+//         if (typeof item === "string") return item;
+//         if (item == null) return "jakis blad";
+//         return (item as any).filename ?? (item as any).src ?? (item as any).path ?? "error";
+//       })
+//       .filter(Boolean);
+//   } catch {
+//     return [];
+//   }
+// })();
 
 const MAX_HEIGHT = 200;
 
 const PhotoMosaic = () => {
 
-  console.log(imageList);
-
   return (
-    <div className="flex flex-wrap justify-center gap-2">
-      {imageList.map((filename, idx) => {
+    <div className="grid grid-flow-col gap-2">
+      {gallery.map((item, idx) => {
+        console.log(item.aspectRatio.toPrecision(2));
         return (
           <div
             key={idx}
             className="rounded-md shadow-md bg-cover bg-center"
             style={{
-              backgroundImage: `url(src/assets/staticImages/${filename})`,
-              width: '200px',
-              aspectRatio: '3 / 2',
+              backgroundImage: `url(staticImages/${item.filename})`,
+              width: 'auto',
+              height: '200px',
+              aspectRatio: item.aspectRatio,
             }}
-          >{filename}</div>
+          >{item.filename}</div>
         );
       })}
     </div>
