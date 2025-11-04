f3a1a67f5726105e1aebdd18ea08ca370999c955
Marcel Zielinski <marziel2001@gmail.com>
2025-11-04 19:09:55 +0100

    Add thumbnails, generator and lightbox

--- portfolio-site/src/components/photoMosaic.tsx
+++ portfolio-site/src/components/photoMosaic.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import galleryImages from "../../public/staticImages/gallery.json";
 
-import { RowsPhotoAlbum } from "react-photo-album";
+import { RowsPhotoAlbum } from "react-photo-album"
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";;
 import "react-photo-album/rows.css";
 
 const photos = galleryImages.map((img) => ({
-  src: `/staticImages/${img.filename}`,
+  src: img.full, // pełny rozmiar zdjęcia
   width: img.width,
   height: img.height,
 }));
+
+const thumbnails = galleryImages.map((img) => ({
+  src: img.thumb, // miniaturka
+  width: img.width,
+  height: img.height,
+}));
+
+
 
 const Gallery = () => <RowsPhotoAlbum spacing={5} photos={photos} targetRowHeight={300} />;
 
@@ -15,8 +32,22 @@ const MAX_HEIGHT = 200;
 
 const PhotoMosaic = () => {
 
+  const [index, setIndex] = useState(-1);
+  
   return (
-    Gallery()
+    <>
+    
+ <RowsPhotoAlbum photos={thumbnails} targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
   )
 };
 
